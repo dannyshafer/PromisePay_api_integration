774190e81506075b81e924e62fa5e7fe7427f7e7
dannyshafer <github@danielshafer.com>
2015-10-15 13:26:24 -0700

    add request session token javascript

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -15,6 +15,26 @@ class TransactionsController < ApplicationController
   # GET /transactions/new
   def new
     @transaction = Transaction.new
+    client = Promisepay::Client.new(username: ENV['PROMISEPAY_USERNAME'], token: ENV['PROMISEPAY_TOKEN'])
+    token = client.tokens.create(:session, {
+      #In this instance, Daniel is the "buyer" and Ben is the "seller" as Daniel is sending money to Ben.
+      current_user_id: '1',
+      item_name: 'Test Item',
+      amount: '100',
+      seller_lastname: 'Harberts',
+      seller_firstname: 'Ben',
+      buyer_lastname: 'Shafer',
+      buyer_firstname: 'Daniel',
+      buyer_country: 'USA',
+      seller_country: 'USA',
+      seller_email: 'd4n@danielshafer.com',
+      buyer_email: 'ben@harberts.com',
+      external_item_id: '1',
+      external_seller_id: '2',
+      external_buyer_id: '1',
+      fee_ids: [],
+      payment_type_id: 2
+    })['token']
   end
 
   # GET /transactions/1/edit
