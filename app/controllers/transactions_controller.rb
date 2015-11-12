568dabcbc7317749c0a545b8a5008e5962033de8
dannyshafer <github@danielshafer.com>
2015-11-12 14:52:39 -0800

    finish embedding the EUI with hardcoded values

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,11 +1,33 @@
 class TransactionsController < ApplicationController
+  require 'securerandom'
+
   def index
-    return "nothing to see here"
   end
 
   def new
     auth = {:username => ENV['PROMISEPAY_USERNAME'], :password => ENV['PROMISEPAY_TOKEN']}
-    @session = HTTParty.get("https://test.api.promisepay.com/request_session_token?current_user_id=1&item_name=Sample+Item&amount=25000&seller_lastname=Seller&seller_firstname=Sally&buyer_lastname=Buyer&buyer_firstname=Bobby&seller_email=sally.seller@promisepay.com&buyer_email=bobby.buyer@promisepay.com&external_item_id=1&external_seller_id=1&external_buyer_id=2&payment_type_id=1&buyer_country=USA&seller_country=USA&fee_ids=", :basic_auth => auth)
+    transaction_info = {
+      "current_user_id" => "1",
+      "payment_type_id" => "3",
+      "fee_ids" => "",
+      "item_name" => "Scooter",
+      "external_item_id" => SecureRandom.hex(7).to_s,
+      "amount" => "25000",
+      "seller_firstname" => "Becky",
+      "seller_lastname" => "McPheeters",
+      "seller_email" => "becky@becky.org",
+      "seller_country" => "USA",
+      "external_seller_id" => "1",
+      "buyer_firstname" => "Daniel",
+      "buyer_lastname" => "Shafer",
+      "buyer_country" => "USA",
+      "buyer_email" => "daniel@daniel.com",
+      "external_buyer_id" => "2"
+    }
+
+    @request_url = ["https://test.api.promisepay.com/request_session_token?current_user_id=", transaction_info["current_user_id"], "&item_name=", transaction_info["item_name"], "&amount=", transaction_info["amount"], "&seller_lastname=", transaction_info["seller_lastname"], "&seller_firstname=", transaction_info["seller_firstname"], "&buyer_lastname=", transaction_info["buyer_lastname"], "&buyer_firstname=", transaction_info["buyer_firstname"], "&seller_email=", transaction_info["seller_email"], "&buyer_email=", transaction_info["buyer_email"], "&external_item_id=", transaction_info["external_item_id"], "&external_seller_id=", transaction_info["external_seller_id"], "&external_buyer_id=", transaction_info["external_buyer_id"], "&payment_type_id=", transaction_info["payment_type_id"], "&buyer_country=", transaction_info["buyer_country"], "&seller_country=", transaction_info["seller_country"], "&fee_ids=", transaction_info["fee_ids"]].join("").to_s
+
+    @session = HTTParty.get(@request_url, :basic_auth => auth)
     @token = @session["token"]
   end
 
