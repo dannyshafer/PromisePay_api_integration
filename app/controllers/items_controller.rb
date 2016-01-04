dc3f994135ea26114df48b5573493cf59fdcebdb
dannyshafer <github@danielshafer.com>
2016-01-04 10:14:19 -0800

    update item form

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -84,6 +84,6 @@ class ItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def item_params
-      params.require(:item).permit(:name, :amount, :buyer_id, :seller_id, :description)
+      params.require(:item).permit(:name, :amount, :buyer_id, :seller_id, :description, :payment_type)
     end
 end
